c403e37127b608b08d0dd2024b66664dfb855f7f
Emmanuel michael <officialimmanuelmichael@gmail.com>
2025-04-21 20:41:45 -0700

    second commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import './Header.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="navigation">
-        <div className="logo"></div>
-        <div className="nav-links">
-            <div>Homepage</div>
-            <div>Features</div>
-            <div>About us</div>
-            <div>Contact us</div>
-            <div>FAQs</div>
+        <div className="logo">
+          <img src="/IMG_3457.jpg" alt="our logo" loading='lazy'/>
         </div>
+        <div className="nav-links">
+        <NavLink to="/" className={({ isActive }) => isActive ? 'link active' : 'link'}>Homepage</NavLink>
+        <NavLink to="/features" className={({ isActive }) => isActive ? 'link active' : 'link'}>Features</NavLink>
+        <NavLink to="/about" className={({ isActive }) => isActive ? 'link active' : 'link'}>About us</NavLink>
+        <NavLink to="/contact" className={({ isActive }) => isActive ? 'link active' : 'link'}>Contact us</NavLink>
+        <NavLink to="/faq" className={({ isActive }) => isActive ? 'link active' : 'link'}>FAQ</NavLink>
+
+        </div>
+        <div className="button">
+          <button>Get Started</button>
+        </div>
+
+      {/* Hero */}
+      <div className="hero-container">
+        <div className="content">
+          <img src="/public/IMG_3457 (2).jpg" alt="" />
+        </div>
+        <div className="content"></div>
+      </div>
     </div>
   )
 }
