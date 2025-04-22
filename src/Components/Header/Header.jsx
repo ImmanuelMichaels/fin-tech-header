845c963d652c8e68b56a40de5c6de65128a332a4
Emmanuel michael <officialimmanuelmichael@gmail.com>
2025-04-21 22:50:03 -0700

    last commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,30 +4,50 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="navigation">
-        <div className="logo">
-          <img src="/IMG_3457.jpg" alt="our logo" loading='lazy'/>
-        </div>
-        <div className="nav-links">
-        <NavLink to="/" className={({ isActive }) => isActive ? 'link active' : 'link'}>Homepage</NavLink>
-        <NavLink to="/features" className={({ isActive }) => isActive ? 'link active' : 'link'}>Features</NavLink>
-        <NavLink to="/about" className={({ isActive }) => isActive ? 'link active' : 'link'}>About us</NavLink>
-        <NavLink to="/contact" className={({ isActive }) => isActive ? 'link active' : 'link'}>Contact us</NavLink>
-        <NavLink to="/faq" className={({ isActive }) => isActive ? 'link active' : 'link'}>FAQ</NavLink>
+    <div className="header-container">
 
-        </div>
-        <div className="button">
-          <button>Get Started</button>
-        </div>
+      <div className="navigation">
+          <div className="logo">
+            <img src="/IMG_3457.jpg" alt="our logo" loading='lazy'/>
+          </div>
+          <div className="nav-links">
+          <NavLink to="/" className={({ isActive }) => isActive ? 'link active' : 'link'}>Homepage</NavLink>
+          <NavLink to="/features" className={({ isActive }) => isActive ? 'link active' : 'link'}>Features</NavLink>
+          <NavLink to="/about" className={({ isActive }) => isActive ? 'link active' : 'link'}>About us</NavLink>
+          <NavLink to="/contact" className={({ isActive }) => isActive ? 'link active' : 'link'}>Contact us</NavLink>
+          <NavLink to="/faq" className={({ isActive }) => isActive ? 'link active' : 'link'}>FAQ</NavLink>
+
+          </div>
+          <div className="button">
+            <button>Get Started</button>
+          </div>
+      </div>
 
       {/* Hero */}
       <div className="hero-container">
-        <div className="content">
-          <img src="/public/IMG_3457 (2).jpg" alt="" />
+        <div className="content" id='hero-img-con'>
+          <img src="/public/IMG_3457 (2).jpg" alt="" loading='lazy' className='hero-image'/>
         </div>
-        <div className="content"></div>
+        <div className="content" id='hero-title-con'>
+          <div className="title">Secure & Seamless Banking at Your <span>Convenience.</span></div>
+          <img src="/line.png" alt="line" className='line-drawing'/>
+        <div className="text">
+          <p>Track expenses in real time. Discover the power of our app, 
+            designed to put you in control of your financial journey
+          </p>
+
+          
+            <button>Get the app</button>
+          
+        </div>
+        </div>
+      </div>
+      <div className="partners">
+        
       </div>
     </div>
+
+    
   )
 }
 
